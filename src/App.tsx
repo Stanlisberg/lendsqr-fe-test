4606c30cc4f00c49d68ddf2f8a547b6784a39be0
Stanlisberg <nnabugo2010@gmail.com>
2023-02-28 12:57:44 +0100

    Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
+import Login from "./components/pages/Login"
+
 function App() {
   return(
-    <div>Hey</div>
+    <div>
+      <Login />
+    </div>
   )
 }
 export default App
---
+++ src/components/pages/Login.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div className='container'>
+        <div className='wrapper'>
+            <div className='login-left'>lendsqr</div>
+            <div className='login-right'>welcome</div>
+        </div>
+    </div>
+  )
+}
+
+export default Login
