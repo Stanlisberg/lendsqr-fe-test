0114e26d30dc36012d91479375146b6c7f3c6f0a
Stanlisberg <nnabugo2010@gmail.com>
2023-02-28 14:24:20 +0100

    build-up

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import lendsqr from '../../icons/lendsqr.svg'
+import pablo from '../../icons/pablo.svg'
+import '../../styles/Login.css'
+
 
 function Login() {
   return (
     <div className='container'>
         <div className='wrapper'>
-            <div className='login-left'>lendsqr</div>
+            <div className='login-left'>lendsqr
+                <img src={lendsqr} />
+                <img src={pablo} />
+               
+            </div>
             <div className='login-right'>welcome</div>
         </div>
     </div>
