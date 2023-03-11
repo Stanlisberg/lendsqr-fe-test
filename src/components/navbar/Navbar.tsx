14695ad4cf27d42549ca67d57eca0f20e8dedaed
Stanlisberg <nnabugo2010@gmail.com>
2023-03-11 22:39:57 +0100

    styling

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { BiSearch} from 'react-icons/bi'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+      <div className='nav-container'>
+        <div className='input-div'>
+         <input type='text' placeholder='Search for anything' />
+
+        </div>
+
+
+      </div>
+    </div>
   )
 }
 
