5ecc10a665a330b28003333c53afe78bc5a2b5aa
Stanlisberg <nnabugo2010@gmail.com>
2023-04-11 09:30:53 +0100

    user-details style modified

--- src/components/sidebar/sidebarPages/UserDetails.tsx
+++ src/components/sidebar/sidebarPages/UserDetails.tsx
@@ -1,23 +1,62 @@
-import React from 'react'
-import{ useEffect, useContext } from 'react'
-import { useParams } from 'react-router-dom'
-import LendsqrContext from '../../context/LendsqrContext'
+import React from "react";
+import { useEffect, useContext } from "react";
+import { useParams } from "react-router-dom";
+import LendsqrContext from "../../context/LendsqrContext";
+import "../../../styles/UserDetails.css"
 
 function UserDetails() {
-  const { getUserDetails, userDetails} = useContext(LendsqrContext);
- const { details }  = useParams();
+  const { getUserDetails, userDetails } = useContext(LendsqrContext);
+  const { details } = useParams();
 
- useEffect(() => {
-  getUserDetails(`https://6270020422c706a0ae70b72c.mockapi.io/lendsqr/api/v1/users/${details}`)
- }, [])
+  useEffect(() => {
+    getUserDetails(
+      `https://6270020422c706a0ae70b72c.mockapi.io/lendsqr/api/v1/users/${details}`
+    );
+  }, []);
 
- console.log(userDetails);
+  console.log(userDetails);
 
   return (
-    <div>
-      UserDetails
+    <div className="details-component">
+      <div className="first-column">
+        <div className="first-row">
+          <p>Back to Users</p>
+        </div>
+        <div className="second row">
+          <p>User Details</p>
+          <div>
+            <div>Blacklist user</div>
+            <div>Active user</div>
+          </div>
+        </div>
+        <div className="third-row">
+          <div className="third-row-wrapper">
+            <div className="first-div"></div>
+            <div className="second-div">
+              <p> Frank Morrison</p>
+              <p>Liu7687yuh9</p>
+            </div>
+            <div className="third-div">
+              <p>user tier</p>
+              <p></p>
+            </div>
+            <div className="forth-div">
+              <p>#2000000</p>
+              <p>providus bank</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="second-column">
+        <div>General Details</div>
+        <div>Documents</div>
+        <div>Bank Details</div>
+        <div>Loans</div>
+        <div>Savings</div>
+        <div>App and System</div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default UserDetails
+export default UserDetails;
