7b833857c399e5732881ef240ce08cb17837a203
Stanlisberg <nnabugo2010@gmail.com>
2023-05-23 06:45:48 +0100

    Feature: Adjusted search-input

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "../../styles/Sidebar.css";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import {
   FaBriefcase,
   FaHome,
